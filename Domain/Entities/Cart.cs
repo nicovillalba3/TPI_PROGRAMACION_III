a46fed364e57427c71fbcd7244ee6a19dc320ada
Nico Villalba <nicolasvillalba3@gmail.com>
2024-07-07 19:00:29 -0300

    Entities

---
+++ Domain/Entities/Cart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Cart
+    {
+        public Product[] ProductList { get; set; }
+        public double TotalPrice { get; set; }
+
+        public int AmountProducts { get; set; }
+
+        public string TypePayment {  get; set; }
+
+        public int Order {  get; set; }
+
+
+    }
+}
---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Product
+    {
+        public string Name {  get; set; }
+
+        public string Id { get; set; }
+
+        public int Stock {  get; set; }
+
+        public double Price { get; set; }
+            
+        public string Image { get; set; }
+
+    }
+}
