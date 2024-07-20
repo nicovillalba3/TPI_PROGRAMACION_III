cb2a487092f4629e7b41af04abd826755f6ecb96
Nico Villalba <nicolasvillalba3@gmail.com>
2024-07-20 20:16:17 -0300

    Admin

--- Domain/Entities/Admin.cs
+++ Domain/Entities/Admin.cs
@@ -8,6 +8,33 @@ namespace Domain.Entities
 {
     public class Admin
     {
-        public string Dni { get; set; }
+        public required string Dni { get; set; }
+    }
+
+    // Metodo para manejar productos
+    public void CreateProduct(List<Product> products, Product product)
+    {
+        products.Add(product);
+    }
+
+    public void EditProduct(List<Product> products, int productId, string name, int stock, double price, string image)
+    {
+        var product = products.FirstOrDefault(p => p.Id == productId);
+        if (product != null)
+        {
+            product.Name = name;
+            product.Image = image;
+            product.Price = price;
+            product.Stock = stock;
+        }
+    }
+
+    public void DeleteProduct(List<Product> products, int productId)
+    {
+        var product = products.FirstOrDefault(p => p.Id == productId);
+        if (product != null)
+        {
+            products.Remove(product);
+        }
     }
 }
--- Domain/Entities/Product.cs
+++ Domain/Entities/Product.cs
@@ -10,13 +10,28 @@ namespace Domain.Entities
     {
         public required string Name {  get; set; }
 
-        public required string Id { get; set; }
+        public required int Id { get; set; }
 
         public required int Stock {  get; set; }
 
         public required double Price { get; set; }
             
         public required string Image { get; set; }
+
+        public Product(string name, string id, int stock, double price, string image)
+        {
+            Name = name;
+
+            Id = id;
+            
+            Stock = stock;
+
+            Price = price;
+
+            Image = image;
+
+        }
+
 
     }
 }
